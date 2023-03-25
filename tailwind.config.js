f07b67f9be0d1871b2a246186bde7a0bf4001f0d
monicamary <poluakanmonica@gmail.com>
2023-03-25 22:36:31 +0800

    uts-kelas-senin-monica

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['Halaman1.html', 'Halaman2.html', 'Halaman3.html' ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
